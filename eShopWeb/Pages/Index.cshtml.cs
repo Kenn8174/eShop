b8e0d18658d5ba9c0b0b407371f7d91e19d48d4a
Kenneth Jessen <kennethjessen1@hotmail.com>
2019-03-28 22:17:43 +0100

    Tilføjet WebUI

---
+++ eShopWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceLayer;
+
+namespace eShopWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IShopService _shopservice;
+
+        public IndexModel(IShopService shopservice)
+        {
+            _shopservice = shopservice;
+        }
+
+        public List<Phone> Phone { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Phone = await _shopservice.GetPhonesAsync();
+        }
+    }
+}
